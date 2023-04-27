a253c0c9202386a85a28bc95687a35ba6ffa7c9d
Max <Asket-axe@mail.ru>
2023-04-27 16:31:30 +0300

    на проверку

--- src/main/java/ru/kata/spring/boot_security/demo/controller/myRestController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/myRestController.java
@@ -1,4 +1,4 @@
-package ru.kata.spring.boot_security.demo.controllers;
+package ru.kata.spring.boot_security.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.Role;
 import ru.kata.spring.boot_security.demo.models.User;
-import ru.kata.spring.boot_security.demo.repository.RoleRepository;
+import ru.kata.spring.boot_security.demo.repositories.RoleRepository;
 import ru.kata.spring.boot_security.demo.service.UserService;
 import java.security.Principal;
 import java.util.List;
